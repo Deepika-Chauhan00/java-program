6110095e8b6a963068f56c0d125884b12eca0740
Deepika-Chauhan00 <deepikachauhan1048@gmail.com>
2025-03-04 10:58:02 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+class odd{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number....");
+        int no=sc.nextInt();
+        if(no%2==0)
+        {
+            System.out.println("even number");
+        }
+        else{
+                        System.out.println("even number");
+
+        }
+    }
+}
